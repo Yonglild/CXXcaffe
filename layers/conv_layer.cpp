a530356c3998c96939a5d9cd687681206432ed9a
15857182156 <1004376731@qq.com>
2021-02-16 12:12:01 +0800

    Add pooling_layer

--- layers/conv_layer.cpp
+++ layers/conv_layer.cpp
@@ -5,12 +5,21 @@
 #include "../include/layers/conv_layer.hpp"
 
 namespace caffe{
-
-
 template <typename Dtype>
 void ConvolutionLayer<Dtype>::compute_output_shape() {
-
+    const int* kernel_shape_data = this->kernel_shape_.cpu_data();
+    const int* stride_data = this->stride_.cpu_data();
+    const int* pad_data = this->pad_.cpu_data();
+    const int* dilation_data = this->dilation_.cpu_data();
+    this->output_shape_.clear();
+    for(int i=0; i < this->num_spatial_axes_; ++i){
+        const int input_dim = this->input_shape(i+1);   // i + 1 to skip channel axis
+        const int kernel_extent = dilation_data[i] * (kernel_shape_data[i] - 1) + 1;
+        const int output_dim = (input_dim + 2 * pad_data[i] - kernel_extent) / stride_data[i] + 1;
+        this->output_shape_.push_back(output_dim);
+    }
 }
+
 template <typename Dtype>
 void ConvolutionLayer<Dtype>::Forward_cpu(const vector<caffe::Blob<Dtype> *> &bottom,
                                           const vector<caffe::Blob<Dtype> *> &top) {
@@ -28,6 +37,4 @@ void ConvolutionLayer<Dtype>::Forward_cpu(const vector<caffe::Blob<Dtype> *> &bo
         }
     }
 }
-
-
 }
